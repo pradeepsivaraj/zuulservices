f8b3180007d55290f6a57ff0c09c4862bc450bbd
M1057071 <M1057071@A2ML36455.mindtree.com>
2020-04-14 16:00:53 +0530

    Added new annotation

--- zuul-gateway/src/main/java/com/zuul/gateway/server/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/zuul/gateway/server/zuulgateway/ZuulGatewayApplication.java
@@ -3,11 +3,14 @@ package com.zuul.gateway.server.zuulgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@SpringBootApplication
+@EnableEurekaClient
 public class ZuulGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
